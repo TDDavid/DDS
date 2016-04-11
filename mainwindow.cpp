f107d438ff84f6fc1ac77252794a61b860e203e7
David Tanko <kalach007@gmail.com>
2016-04-11 19:21:49 +0200

    Uprava matice m v riadku 19

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,11 +12,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     MatrixXd m(2,2);
-    /*m(0,0) = 1;
+    m(0,0) = 1;
     m(1,0) = 3;
     m(0,1) = 2;
-    m(1,1) = 4;*/
-    m = {(1,2),(3,4)};
+    m(1,1) = 4;
+   // m = {(1,2),(3,4)};
     MatrixXd n(2,1);
     n(0,0) = 0;
     n(1,0) = 1;
